b64e5e10b0d39978b1948d55504eec83fc7be119
romanovichim <romanovich.i.m@gmail.com>
2023-09-02 20:35:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),nodePolyfills()],
-  base: "tonspeedrun",
+  base: "https://tonspeedrun.com/",
 })
